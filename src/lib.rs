34eaac43d44a34c1612527dff62ccdaae77922e5
chrlsia <xrsia@otenet.gr>
2025-04-08 11:47:09 +0300

    New test failed

--- src/lib.rs
+++ src/lib.rs
@@ -25,25 +25,41 @@ impl Museum{
     }
 }
 
+// compiler do not include the test code with
+// the binary file you build or run
+// in other words execute the code below
+// when we run tests
+#[cfg(test)]
 // we can create a mod and put the test functions in it
 // the name of mod can can be given arbitralily from us
 mod tests{
+
+    // better to bring into to scope
+    // all of the above module
+    // * is the globe operator
+    use super::*;
+
     #[test]
     fn museum_sells_ticket_to_increase_revenue(){
         // create an new mut instance of Museum
         // invokesell_ticket method
         // assert result
 
-        // what about crate keyword?
-        // crate keyword provides an absolute path
-        // starting from the root which is the lib crate
-        // so the code above
-        let mut museum = crate::Museum::new();
+        // and now we can call Museum directly
+        let mut museum = Museum::new();
         museum.sell_ticket();
         assert_eq!(museum.value, 25);
     }
-}
 
-// in the mod tests we can have fn without the #[test]
-// attribute just as helpful function for the test functions
-// compiler can accept it
+
+}#[test]
+    fn museum_can_sell_multiple_tickets(){
+        let mut museum = Museum::new();
+        museum.sell_ticket();
+        museum.sell_ticket();
+        assert_eq!(museum.value, 25); // there is a failure
+    }
+
+// we we have multiple tests to run rust runs then in
+// multiple threads. so if one test fails the program 
+// does not exit but still runs the rest of the tests.
